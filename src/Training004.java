4743eebaaeb7242b4eb0249a7c8b62d7261fa02b
liuwei <Liu.Wei.Wayne@cn.ricoh.com>
2020-01-09 09:08:22 +0800

    追加004&005

---
+++ src/Training004.java
@@ -0,0 +1,18 @@
+class Training004{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+// 各种常用排序算法
+class Sort{
+    // 冒泡排序
+
+    // 快速排序
+
+    // 选择排序
+
+    // 插入排序
+
+    // 堆排序
+}
---
+++ src/Training005.java
@@ -0,0 +1,13 @@
+class Training005{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+class BinaryTree{
+    // 前序递归
+
+    // 中序递归
+
+    // 后序递归
+}
