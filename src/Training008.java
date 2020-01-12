926a9d6199cd2d4c9fdf363e7a8a5ab38df9ef5f
liuwei <sxzj365@hotmail.com>
2020-01-12 18:43:25 +0800

    判断链表是否有环

--- src/Training008.java
+++ src/Training008.java
@@ -17,28 +17,44 @@ class Training008 {
         lSolution.insert(c, d);
         ListNode e = new ListNode(5);
         lSolution.insert(d, e);
+        e.pNextListNode = b;
 
-        lSolution.printList(head);
+//        lSolution.printList(head);
+        System.out.println(lSolution.isCycle(head));
     }
 }
 
-class ListSolution{
+class ListSolution {
     // 从尾到头打印链表（用stack来实现）
-    void printList(ListNode headListNode){
+    void printList(ListNode headListNode) {
         Stack<ListNode> stack = new Stack<ListNode>();
-        ListNode pListNode =headListNode.pNextListNode;
-        while(pListNode!=null){
+        ListNode pListNode = headListNode.pNextListNode;
+        while (pListNode != null) {
             stack.push(pListNode);
-            pListNode=pListNode.pNextListNode;
+            pListNode = pListNode.pNextListNode;
         }
-        while(!stack.empty()){
-            System.out.print(stack.pop().value+" ");
+        while (!stack.empty()) {
+            System.out.print(stack.pop().value + " ");
         }
     }
 
-    void insert(ListNode lasListNode,ListNode pListNode){
-        lasListNode.pNextListNode=pListNode;
-        pListNode.pNextListNode=null;
+    // 判断链表是否有环
+    boolean isCycle(ListNode headListNode) {
+        ListNode p1 = headListNode;
+        ListNode p2 = headListNode;
+        while (p2 != null && p2.pNextListNode != null) {
+            p1 = p1.pNextListNode;
+            p2 = p2.pNextListNode.pNextListNode;
+            while (p1 == p2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void insert(ListNode lasListNode, ListNode pListNode) {
+        lasListNode.pNextListNode = pListNode;
+        pListNode.pNextListNode = null;
     }
 }
 
@@ -46,7 +62,7 @@ class ListNode {
     int value;
     ListNode pNextListNode;
 
-    ListNode(){
+    ListNode() {
         value = 0;
         pNextListNode = null;
     }
