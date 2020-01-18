55e79901ca5eec3d08c019b148d92cfe6cd6d681
liuwei <sxzj365@hotmail.com>
2020-01-18 13:38:24 +0800

    线程同步问题测试

---
+++ src/Training011.java
@@ -0,0 +1,23 @@
+class Training001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        threadtest t = new threadtest();
+        Thread t1 = new Thread(t);
+        Thread t2 = new Thread(t);
+//        t1.start();
+//        t2.start();
+        t.run();
+        }
+}
+
+// 线程同步问题测试
+class threadtest implements Runnable{
+    int args=0;
+    @Override
+    public void run() {
+        for(int i=0;i<100;i++){
+            System.out.println(Thread.currentThread().getName()+":"+args);
+            args++;
+        }
+    }
+}
