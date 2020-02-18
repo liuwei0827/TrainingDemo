4a884a61c01f85a2ad812cafc0c7c1c1d3085a7b
liuwei <Liu.Wei.Wayne@cn.ricoh.com>
2020-02-18 10:05:08 +0800

    全组合问题

--- src/Training015.java
+++ src/Training015.java
@@ -3,8 +3,8 @@ class Training015 {
         System.out.println("Hello World.");
         int[] a = { 1, 2, 3 };
         FullSort fs = new FullSort();
-        fs.fullSort(a, 0, a.length - 1);
-
+        // fs.fullSort(a, 0, a.length - 1);
+        fs.combination(a);
     }
 }
 
@@ -30,7 +30,29 @@ class FullSort {
         src[a] = src[b];
         src[b] = tmp;
     }
+
     // 全组合问题
+    void combination(int[] src) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 1; i <= src.length; i++) {
+
+            combination(src, i, stringBuilder, 0);
+        }
+    }
+
+    void combination(int src[], int num, StringBuilder sBuilder, int start) {
+        if (num == 0) {
+            System.out.println(sBuilder);
+        } else {
+            if (start == src.length) {
+                return;
+            }
+            sBuilder.append(src[start]);
+            combination(src, num - 1, sBuilder, start + 1);
+            sBuilder.deleteCharAt(sBuilder.length() - 1);
+            combination(src, num, sBuilder, start + 1);
+        }
+    }
 
     // 八皇后问题
 }
