99cc826f25968530a57aad212c529190528e7630
liuwei0827 <sxzj365@hotmail.com>
2020-03-16 23:15:22 +0800

    二叉树镜像

--- src/Training018.java
+++ src/Training018.java
@@ -1,12 +1,43 @@
 class Training018 {
     // 二叉树合集
     public static void main(String[] args) {
+        TreeNode d = new TreeNode(4, null, null);
+        TreeNode e = new TreeNode(5, null, null);
+        TreeNode f = new TreeNode(6, null, null);
+        TreeNode g = new TreeNode(7, null, null);
+        TreeNode b = new TreeNode(2, d, e);
+        TreeNode c = new TreeNode(3, f, g);
+        TreeNode a = new TreeNode(1, b, c);
+
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.pre_order(a);
+        System.out.println("\n");
+        BTsolution bTsolution=new BTsolution();
+        bTsolution.BTSMirror(a);
+        binaryTree.pre_order(a);
+
+        System.out.println("\n");
         System.out.println("Hello World!");
     }
 }
 
-class solution {
+class BTsolution {
     // 二叉树镜像
+    void BTSMirror(TreeNode rTree) {
+        if (rTree == null) {
+            return;
+        }
+        TreeNode tmpBinaryTree = rTree.LeftNode;
+        rTree.LeftNode = rTree.RightNode;
+        rTree.RightNode = tmpBinaryTree;
+
+        if (rTree.LeftNode != null) {
+            BTSMirror(rTree.LeftNode);
+        }
+        if (rTree.RightNode != null) {
+            BTSMirror(rTree.RightNode);
+        }
+    }
 
     // 对称二叉树
 
