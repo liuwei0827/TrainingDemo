485187ac051440b2d038b4c18814e9879324b10d
liuwei <Liu.Wei.Wayne@cn.ricoh.com>
2020-03-27 09:21:48 +0800

    斐波那契数列

--- src/Training002.java
+++ src/Training002.java
@@ -31,13 +31,13 @@ class Fb {
      */
     public void setOutput(int n) {
         for (int p = 0; p < n; p++) {
-            output[p] = solution2(p);
+            output[p] = solution2(p+1);
         }
     }
 
     // 递归
     int solution1(int n) {
-        if (n == 0 || n == 1) {
+        if (n == 1 || n == 2) {
             return 1;
         } else if (n > 1) {
             return solution1(n - 1) + solution1(n - 2);
@@ -48,13 +48,13 @@ class Fb {
 
     // 非递归
     int solution2(int n) {
-        if (n == 0 || n == 1) {
+        if (n == 1 || n == 2) {
             return 1;
         }
         int result = 1;
         int preResult = 1;
         int prePreRsult = 1;
-        if (n > 1) {
+        if (n > 2) {
             for (int i = 0; i < n - 1; i++) {
                 result = preResult + prePreRsult;
                 prePreRsult = preResult;
