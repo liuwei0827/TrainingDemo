96d6fdb02174fea9c52e129981dbd58ae9ce74bb
liuwei0827 <sxzj365@hotmail.com>
2020-04-06 20:18:36 +0800

    用两个栈实现队列

--- src/Training019.java
+++ src/Training019.java
@@ -1,8 +1,30 @@
+import java.util.Stack;
+
 class Training019{
     public static void main(String[] args) {
-        int[] tmp = new int[5];
-        for(int i=0;i<5;i++){
-            System.out.print(tmp[i]);
+        System.out.println("Hello World!");
+    }
+}
+
+// 用两个栈实现队列
+class MyQueue{
+    private Stack<Integer> stackA =new Stack<Integer>();
+    private Stack<Integer> stackB =new Stack<Integer>();
+    public void enQueue(int element){
+        stackA.push(element);
+    }
+    public Integer deQueue(int element){
+        if(stackB.isEmpty()){
+            if(stackA.isEmpty()){
+                return null;
+            }
+            transfer();
+        }
+        return stackB.pop();
+    }
+    public void transfer(){
+        while(!stackA.isEmpty()){
+            stackB.push(stackA.pop());
         }
     }
 }
